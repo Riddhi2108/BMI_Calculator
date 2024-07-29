8e0a8ca9471d6fe5bffddb6ab4c57bb38bf6cd67
Riddhi Agarwal <riddhiagarwal654@gmail.com>
2024-07-30 01:32:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,46 @@
-const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-function ageCalculate() {
-    let today = new Date();
-    let inputDate = new Date(document.getElementById("date-input").value);
-    let birthMonth, birthDate, birthYear;
-    let birthDetails = {
-        date: inputDate.getDate(),
-        month: inputDate.getMonth() + 1,
-        year: inputDate.getFullYear()
-    }
-    let currentYear = today.getFullYear();
-    let currentMonth = today.getMonth() + 1;
-    let currentDate = today.getDate();
+const btnElem = document.querySelector(".HTMLbuttonElement");
 
-    leapChecker(currentYear);
 
-    if (
-        birthDetails.year > currentYear ||
-        (birthDetails.month > currentMonth && birthDetails.year == currentYear) ||
-        (birthDetails.date > currentDate && birthDetails.month == currentMonth && birthDetails.year == currentYear)
-    ) {
-        alert("Not Born Yet");
-        displayResult("-", "-", "-");
-        return;
-    }
 
-    birthYear = currentYear - birthDetails.year;
-
-    if (currentMonth >= birthDetails.month) {
-        birthMonth = currentMonth - birthDetails.month;
-    }
-    else {
-        birthYear--;
-        birthMonth = 12 + currentMonth - birthDetails.month;
-    }
-
-    if (currentDate >= birthDetails.date) {
-        birthDate = currentDate - birthDetails.date;
-    }
-    else {
-        birthMonth--;
-        let days = months[currentMonth - 2];
-        birthDate = days + currentDate - birthDetails.date;
-        if (birthMonth < 0) {
-            birthMonth = 11;
-            birthYear--;
-        }
-    }
-    displayResult(birthDate, birthMonth, birthYear);
+function calculateDOB() {
+  const UserDOB = document.querySelector("#birthdate").value;
+  // console.log(UserDOB);
+  if (UserDOB === "") {
+    alert("Please Enter the DOB !!!");
+  }
+  else {
+    const resultAGE = calcuateResult(UserDOB);
+    const ShowOutput = document.querySelector("#result");
+    ShowOutput.textContent = resultAGE;
+  }
 }
 
-function displayResult(bDate, bMonth, bYear) {
-    document.getElementById("years").textContent = bYear;
-    document.getElementById("months").textContent = bMonth;
-    document.getElementById("days").textContent = bDate;
+function calcuateResult(UserDOB) {
+  const currentDate = new Date();
+  const userBirthDate = new Date(UserDOB);
+  let age = currentDate.getFullYear() - userBirthDate.getFullYear();
+  if (age < 0) {
+    return "You are not Born 😑";
+  }
+
+  if (userBirthDate.getMonth() > currentDate.getMonth()) {
+    age--;
+  }
+  else if (currentDate.getMonth() === userBirthDate.getMonth()) {
+    if (currentDate.getDate() > userBirthDate.getDate()) {
+      if (age > 0)
+        age--;
+      else
+        return "You are not Born 😑";
+    }
+    else if (currentDate.getDate() === userBirthDate.getDate()) {
+      return "Happy birthday to you 🎉 you are " + age + " year old";
+    }
+
+  }
+  return "your age is " + age;
 }
 
-function leapChecker(year) {
-    if (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)) {
-        months[1] = 29;
-    }
-    else {
-        months[1] = 28;
-    }
-}
+
+btnElem.addEventListener("click", calculateDOB);

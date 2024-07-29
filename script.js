07293cb959766f6dde1a58c9baac793654e175d9
Riddhi Agarwal <riddhiagarwal654@gmail.com>
2024-07-30 01:35:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,39 @@
-
-const btnElem = document.querySelector(".HTMLbuttonElement");
-
-
-
-function calculateDOB() {
-  const UserDOB = document.querySelector("#birthdate").value;
-  // console.log(UserDOB);
-  if (UserDOB === "") {
-    alert("Please Enter the DOB !!!");
-  }
-  else {
-    const resultAGE = calcuateResult(UserDOB);
-    const ShowOutput = document.querySelector("#result");
-    ShowOutput.textContent = resultAGE;
-  }
+function femaleClicked() {
+  var element = document.getElementByClassName("female");
+  element.classList.add("female-clicked");
+  element.classList.removeClass("female");
 }
 
-function calcuateResult(UserDOB) {
-  const currentDate = new Date();
-  const userBirthDate = new Date(UserDOB);
-  let age = currentDate.getFullYear() - userBirthDate.getFullYear();
-  if (age < 0) {
-    return "You are not Born 😑";
-  }
-
-  if (userBirthDate.getMonth() > currentDate.getMonth()) {
-    age--;
-  }
-  else if (currentDate.getMonth() === userBirthDate.getMonth()) {
-    if (currentDate.getDate() > userBirthDate.getDate()) {
-      if (age > 0)
-        age--;
-      else
-        return "You are not Born 😑";
-    }
-    else if (currentDate.getDate() === userBirthDate.getDate()) {
-      return "Happy birthday to you 🎉 you are " + age + " year old";
-    }
-
-  }
-  return "your age is " + age;
+function maleClicked() {
+  var element = document.getElementByClassName("male");
+  element.classList.add("male-clicked");
+  element.classList.removeClass("male");
 }
 
+function calculateBmi() {
+var weight = document.bmiForm.weight.value
+var height = document.bmiForm.height.value
 
-btnElem.addEventListener("click", calculateDOB);
+if(weight > 0 && height > 0){
+var finalBmi = weight/(height/100*height/100)
+
+finalBmi = Math.floor(finalBmi);
+
+
+document.getElementById('result-text').innerHTML = finalBmi
+
+if (finalBmi < 18.5){
+document.getElementById('meaning').innerHTML = "You are underweight."
+
+} else if(finalBmi > 18.5 && finalBmi < 25){
+document.getElementById('meaning').innerHTML = "You are healthy."
+
+} else if(finalBmi > 25){
+document.getElementById('meaning').innerHTML = "You are overweight."
+}
+
+} else{
+document.getElementById('meaning').innerHTML = "Please Fill in everything correctly"
+}
+
+}
